226f1faf3340359272928fc201d598057f632d9e
KawaiiKraken <111396148+KawaiiKraken@users.noreply.github.com>
2024-10-21 22:41:23 -0400

    generics

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,26 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn derive_from_bytes(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let struct_name = input.ident;
+    let generics = &input.generics;
+
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let expanded = quote! {
-        impl #struct_name {
-            pub fn from_bytes(value: &[u8]) -> #struct_name {
+        impl #impl_generics #struct_name #ty_generics #where_clause {
+            pub fn from_bytes(value: &[u8]) -> Self {
                 assert!(
-                    value.len() >= std::mem::size_of::<#struct_name>(),
+                    value.len() >= std::mem::size_of::<Self>(),
                     "`value` should be at least the size of T"
                 );
 
-                let mut result: #struct_name = unsafe { std::mem::zeroed() };
-                let result_pointer: *mut #struct_name = &mut result;
+                let mut result: Self = unsafe { std::mem::zeroed() };
+                let result_pointer: *mut Self = &mut result;
 
                 unsafe {
                     std::ptr::copy_nonoverlapping(
                         value.as_ptr() as *const u8,
                         result_pointer as *mut u8,
-                        std::mem::size_of::<#struct_name>()
+                        std::mem::size_of::<Self>()
                     );
                 }
 
